406ea53cbf57063084e9fe91faf599dd953a2e1b
Salakar <mike.diarmid@gmail.com>
2019-02-17 07:35:54 +0000

    [tests] android emulator tweaks

--- .circleci/scripts/wait-for-avd.sh
+++ .circleci/scripts/wait-for-avd.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-bootanim=""
+boot=""
 
 echo "Waiting for AVD to finish booting"
 export PATH=$(dirname $(dirname $(command -v android)))/platform-tools:$PATH
 
-until [[ "$bootanim" =~ "stopped" ]]; do
-  sleep 10
-  bootanim=$(adb -e shell getprop init.svc.bootanim 2>&1)
+until [[ "$boot" =~ "1" ]]; do
+  sleep 5
+  boot=$(adb -e shell getprop sys.boot_completed 2>&1)
 done
 
 # extra time to let the OS settle
-sleep 25
+sleep 15
+
+adb shell settings put global window_animation_scale 0
+adb shell settings put global transition_animation_scale 0
+adb shell settings put global animator_duration_scale 0
 
 echo "Android Virtual Device is now ready."
--- example/e2e/init.js
+++ example/e2e/init.js
@@ -12,7 +12,7 @@ const detox = require('detox');
 const config = require('../../package.json').detox;
 const adapter = require('detox/runners/jest/adapter');
 
-jest.setTimeout(120000);
+jest.setTimeout(300000);
 jasmine.getEnv().addReporter(adapter);
 
 beforeAll(async () => {
