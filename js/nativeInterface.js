d3882c1be7e5d86cad481f92ba536c387b345b53
Matt Oakes <hello@mattoakes.net>
2019-03-22 15:00:39 +0000

    feat: Throw an error if the native module is null with steps to help fix the issue (#33)

--- js/nativeInterface.js
+++ js/nativeInterface.js
@@ -12,6 +12,17 @@ import {NativeEventEmitter, NativeModules} from 'react-native';
 
 const {RNCNetInfo} = NativeModules;
 
+// Produce an error if we don't have the native module
+if (!RNCNetInfo) {
+  throw new Error(`@react-native-community/netinfo: NativeModule.RNCNetInfo is null. To fix this issue try these steps:
+
+• Run \`react-native link @react-native-community/netinfo\` in the project root.
+• Rebuild and re-run the app.
+• If you are using CocoaPods on iOS, run \`pod install\` in the \`ios\` directory and then rebuild and re-run the app. You may also need to re-open Xcode to get the new pods.
+
+If none of these fix the issue, please open an issue on the Github repository: https://github.com/react-native-community/react-native-netinfo`);
+}
+
 /**
  * We export the native interface in this way to give easy shared access to it between the
  * JavaScript code and the tests
