8aa15b75da4c36129f950f4117fa4bd4401a07b4
Matt Oakes <hello@mattoakes.net>
2019-04-30 09:31:28 -0700

    Upgrade configs to get ESLint passing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,21 @@
+const typescriptEslintRecommended = require('@typescript-eslint/eslint-plugin/dist/configs/recommended.json');
+const typescriptEslintPrettier = require('eslint-config-prettier/@typescript-eslint');
+
 module.exports = {
   extends: ["@react-native-community"],
   overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      // Apply the recommended Typescript defaults and the prettier overrides to all Typescript files
+      rules: Object.assign(typescriptEslintRecommended.rules, typescriptEslintPrettier.rules),
+    },
     {
       files: ["**/__tests__/**/*.ts", "**/*.spec.ts"],
       env: {
         jest: true
       },
       rules: {
+        // Turn off rules which are useless and annoying for unit test files
         "@typescript-eslint/explicit-function-return-type": "off"
       }
     }
