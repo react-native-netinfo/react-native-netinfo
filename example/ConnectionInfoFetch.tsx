e4db49ff6c05a8a5651e1dc20f03449de175898e
Matt Oakes <hello@mattoakes.net>
2019-10-06 11:00:39 +0100

    ADd a test case for fetching the network info once

---
+++ example/ConnectionInfoFetch.tsx
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+import * as React from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
+import NetInfo, {NetInfoSubscription} from '../src';
+
+interface State {
+  connectionInfo: string;
+}
+
+export default class ConnectionInfoCurrent extends React.Component<{}, State> {
+  _subscription: NetInfoSubscription | null = null;
+
+  state = {
+    connectionInfo: 'Tap to get current state',
+  };
+
+  componentDidMount() {
+    this._fetchState();
+  }
+
+  _fetchState = async () => {
+    const state = await NetInfo.fetch();
+    this.setState({
+      connectionInfo: JSON.stringify(state),
+    });
+  };
+
+  render() {
+    return (
+      <View>
+        <TouchableOpacity onPress={this._fetchState}>
+          <Text>{this.state.connectionInfo}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
