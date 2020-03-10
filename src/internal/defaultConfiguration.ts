4e3e9813fbae89013bbeee6470b005b6d923e022
Matt Oakes <hello@mattoakes.net>
2020-03-10 10:50:05 +0000

    fix: Ensure the default timeouts are the correct way around

--- src/internal/defaultConfiguration.ts
+++ src/internal/defaultConfiguration.ts
@@ -2,7 +2,7 @@ export default {
   reachabilityUrl: 'https://clients3.google.com/generate_204',
   reachabilityTest: (response: Response): Promise<boolean> =>
     Promise.resolve(response.status === 204),
-  reachabilityShortTimeout: 60 * 1000, // 60s
-  reachabilityLongTimeout: 5 * 1000, // 5s
+  reachabilityShortTimeout: 5 * 1000, // 5s
+  reachabilityLongTimeout: 60 * 1000, // 60s
   reachabilityRequestTimeout: 15 * 1000, // 15s
 };
