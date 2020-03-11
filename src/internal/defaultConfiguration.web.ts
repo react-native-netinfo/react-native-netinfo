20f0ecd1abe0024f56f6e573a6333035f5ed0a8b
ifsnow <ifsnow@naver.com>
2020-03-11 22:49:22 +0900

    fix(web): Ensure the default timeouts are correct (#322 by @ifsnow)

--- src/internal/defaultConfiguration.web.ts
+++ src/internal/defaultConfiguration.web.ts
@@ -2,7 +2,7 @@ export default {
   reachabilityUrl: '/',
   reachabilityTest: (response: Response): Promise<boolean> =>
     Promise.resolve(response.status === 200),
-  reachabilityShortTimeout: 60 * 1000, // 60s
-  reachabilityLongTimeout: 5 * 1000, // 5s
+  reachabilityShortTimeout: 5 * 1000, // 5s
+  reachabilityLongTimeout: 60 * 1000, // 60s  
   reachabilityRequestTimeout: 15 * 1000, // 15s
 };
