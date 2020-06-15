8cf3b16434fc1649cc09319a2f99da532ddfca58
Ricardo Gomes <ricardo@heptasoft.com>
2020-06-15 12:42:25 +0100

    fix: added strength wifi property to typescript

--- src/internal/nativeModule.web.ts
+++ src/internal/nativeModule.web.ts
@@ -183,6 +183,7 @@ const getCurrentState = (
       details: {
         isConnectionExpensive,
         ssid: null,
+        strength: null,
         ipAddress: null,
         subnet: null,
         frequency: null,
