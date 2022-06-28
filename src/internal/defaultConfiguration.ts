3f5badd41258adeef23d524934743c482f82c692
CaptainJeff <CaptainJeff@users.noreply.github.com>
2022-06-28 12:27:52 -0400

    feat: add support for reachabilityMethod to specify GET or HEAD (#610)

--- src/internal/defaultConfiguration.ts
+++ src/internal/defaultConfiguration.ts
@@ -2,6 +2,7 @@ import * as Types from './types';
 
 const DEFAULT_CONFIGURATION: Types.NetInfoConfiguration = {
   reachabilityUrl: 'https://clients3.google.com/generate_204',
+  reachabilityMethod: 'HEAD',
   reachabilityTest: (response: Response): Promise<boolean> =>
     Promise.resolve(response.status === 204),
   reachabilityShortTimeout: 5 * 1000, // 5s
